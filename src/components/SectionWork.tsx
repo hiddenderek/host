0851a8b86b82e938b2d6eb2a9575ebdd9a2bf230
derek <hiddenderek@gmail.com>
2022-05-01 22:04:04 -0500

    tweaked css

--- src/components/SectionWork.tsx
+++ src/components/SectionWork.tsx
@@ -3,8 +3,8 @@ import { useAppDispatch, useAppSelector } from '../app/hooks'
 import { setSectionPosition } from '../features/pageScroll/pageScroll-slice'
 import Card from './Card'
 const cardData = [
-    {name: 'Game Creator', image: '', text: 'No text yet.'}, 
-    {name: 'Oasis Software Ecosystem', image: '', text: 'No text yet.'}
+    {name: 'Game Creator', image: 'images/gameCreator.png', text: 'A fully functional game editor that allows you to log in, share your games, and play others.', tags: ['Node.js', 'Postgres', 'React', 'Redux']}, 
+    {name: 'Oasis Software Ecosystem', image: 'images/oasis.png', text: 'A software ecosystem built to automate the generation of user interfaces used to control air and water systems of enterprise buildings.', tags: ['Node.js', 'Postgres', 'React', 'Redux']}
 ]
 
 function SectionWork () {
