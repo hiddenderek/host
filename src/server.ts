bbe0323ba7b19407a443eede548a12b893042fa6
derek <hiddenderek@gmail.com>
2022-06-06 17:02:45 -0500

    added https server and began work on mobile version

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,28 @@
 import express from 'express'
 import config from './config'
 import processEnv from 'dotenv'
+import https from 'https'
+import fs from 'fs'
+import useragent from 'express-useragent'
 processEnv.config()
 import serverRenderer from './renderers/serverRenderer'
 const app = express()
 app.use(express.static('public'))
+app.use(useragent.express());
 app.set('view engine', 'ejs')
 
 app.get('/*', async (req: any, res: any) => {
     let contentGet = serverRenderer()
     console.log(contentGet.initialContent)
     console.log('hmmmm')
-    res.render('index', { data: contentGet.initialContent });
+    res.render('index', { data: contentGet.initialContent, isMobile: req.useragent.isMobile, isDesktop: req.useragent.isDesktop });
 })
 
-app.listen(config.port, function listenHandler() {
-    console.info(`Running on ${config.port}`)
-})
+https.createServer({
+    key: fs.readFileSync("./src/key.pem"),
+    cert: fs.readFileSync("./src/cert.pem"),
+  },app).listen(config.port, function listenHandler() {
+      console.info(`Running on ${config.port}`)
+  })
 
 export default app
